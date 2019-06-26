7d8c8dd97f00746e10e8e1d79a1734bce768f355
Aliya07 <50068626+Aliya07@users.noreply.github.com>
2019-06-26 10:00:50 +0530

    Add files via upload

---
+++ express.js
@@ -0,0 +1,14 @@
+var express=require('express');
+var app=express();
+var productfunction=function(req,res);
+{
+var product=[{
+var productId:1,
+var pname:"aliya",
+var quantity:500,
+var price:1000
+}];
+res.send(product);
+}
+app.get('/product',productfunction);
+var server=app.listen(3000);
